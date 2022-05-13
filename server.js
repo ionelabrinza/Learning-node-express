0f40a98c825d77330cdf6614083b237abfcb27c2
Nicoleta Brinza <ionela.brinza@yahoo.com>
2022-05-13 01:33:48 -0500

    Express Router

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const campsiteRouter = require('./routes/campsiteRouter');
+
 
 const hostname = 'localhost';
 const port = 3000;
@@ -7,6 +9,8 @@ const port = 3000;
 const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
+
+app.use('/campsites', campsiteRouter);
 
 app.all('/campsites', (req, res, next) => {
     res.statusCode = 200;
